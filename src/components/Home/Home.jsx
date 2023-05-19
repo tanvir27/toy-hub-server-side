065f93e390a2ac6b5d32e6c7e34d22adf622bf45
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-05-19 15:54:13 +0600

    gallery section and banner section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
-
+import Carousel from "react-bootstrap/Carousel";
 import { Container, Row, Col, Image, Form, Button } from "react-bootstrap";
 import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
@@ -12,26 +12,134 @@ const Home = () => {
     <div className="bg-color">
       <div className="container home-section pt-lg-5">
         {/* banner section start */}
-       
+        <div className="container mb-3 mb-lg-5 ">
+          <div className="row ">
+            <div className="col-md-6">
+              <div className="px-0 px-lg-5 text-color">
+                <h1 className="text-center pt-2">
+                  Discover Endless Adventures with Toy Hub
+                </h1>
+                <p>
+                  Experience the thrill of racing and collecting toy cars in our
+                  exclusive toy car marketplace.
+                </p>
+                <p>
+                  Explore our vast collection of toy cars and dive into a world
+                  of thrilling racing and imaginative play. At our toy cars
+                  marketplace, you'll find an extensive range of high-quality
+                  toy cars.
+                </p>
+                <div className="text-center py-3">
+                  <button type="button" className="btn btn-outline-primary">
+                    EXPLORE NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 px-0">
+              <div className="rounded-5">
+                <Carousel>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 img-fluid rounded-5"
+                      src="https://images.unsplash.com/photo-1642761030731-1e57677c3b97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTgxfHx0b3klMjBzcG9ydHMlMjBjYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 img-fluid rounded-5"
+                      src="https://images.unsplash.com/photo-1627808838459-6198a4bd8876?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjA2fHx0b3klMjBzcG9ydHMlMjBjYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                      alt="Second slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 img-fluid rounded-5"
+                      src="https://images.unsplash.com/photo-1612527713429-25bdfb34a2ec?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg5fHx0b3klMjBzcG9ydHMlMjBjYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* banner section end */}
 
-        {/* chefs section  */}
-        {/* <div className="text-center pt-4">
-          <h1 className="fw-bold text-secondary py-2">
-            Meet Our Talented Chefs
-          </h1>
-        </div> */}
-        {/* chefs section  card*/}
-        {/* <div className="row row-cols-1 row-cols-md-3 mt-2">
-          {loader.map((data) => (
-            <ChefDetails data={data} key={data.id}></ChefDetails>
-          ))}
-        </div> */}
-        {/* chefs section end here  */}
+        {/* gallery  section start*/}
 
-        {/* about us section start*/}
-        
-        {/* about us section end */}
+        <div className=" my-2 my-lg-5">
+          <h1 className="text-center text-color fw-bold">
+            <i>Gallery</i>
+          </h1>
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12 shadow py-3 body-color rounded">
+              <div className="card shadow">
+                <img
+                  className="img-fluid rounded"
+                  style={{ height: "250px" }}
+                  src="https://img.freepik.com/free-photo/purple-white-race-car-with-word-speed-side_1340-25737.jpg?size=626&ext=jpg&uid=R101497129&ga=GA1.2.1449908384.1679952179&semt=ais"
+                  alt="Card 1"
+                />
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12 shadow py-3 body-color rounded">
+              <div className="card shadow">
+                <img
+                  className=" img-fluid rounded"
+                  style={{ height: "250px" }}
+                  src="https://img.freepik.com/free-photo/3d-render-f1-racing-car_1048-5611.jpg?size=626&ext=jpg&uid=R101497129&ga=GA1.2.1449908384.1679952179&semt=ais"
+                  alt="Card 1"
+                />
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12 shadow py-3 body-color rounded">
+              <div className="card shadow">
+                <img
+                  className=" img-fluid rounded"
+                  style={{ height: "250px" }}
+                  src="https://img.freepik.com/free-photo/blue-car-with-white-stripe-that-says-ford_1340-37094.jpg?size=626&ext=jpg&uid=R101497129&ga=GA1.2.1449908384.1679952179&semt=ais"
+                  alt="Card 1"
+                />
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-12 shadow py-3 body-color rounded">
+              <div className="card shadow">
+                <img
+                  className="img-fluid rounded"
+                  style={{ height: "250px" }}
+                  src="https://images.unsplash.com/photo-1676763092563-628ad69c226b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRveSUyMHRydWNrJTIwY2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Card 1"
+                />
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12 shadow py-3 body-color rounded">
+              <div className="card shadow">
+                <img
+                  className=" img-fluid rounded"
+                  style={{ height: "250px" }}
+                  src="https://images.unsplash.com/photo-1663885193694-bbeae8c95b2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHRveSUyMHRydWNrJTIwY2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Card 1"
+                />
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12 shadow py-3 body-color rounded">
+              <div className="card shadow">
+                <img
+                  className=" img-fluid rounded"
+                  style={{ height: "250px" }}
+                  src="https://images.unsplash.com/photo-1564518073728-d947ff17f249?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fHRveSUyMHRydWNrJTIwY2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Card 1"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* gallery  section end */}
 
         {/* contact us section  */}
 
